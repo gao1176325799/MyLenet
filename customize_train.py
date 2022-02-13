96b749685376e59fe74638b3947af18c853192e1
Victor-Gao <gxh.vic@gmail.com>
2022-02-13 11:01:02 -0400

    Update customize_train.py
    restored, if the with is not at the first place, it gave me error as  "Unindent amount does not match previous indent"

--- customize_train.py
+++ customize_train.py
@@ -20,18 +20,17 @@ for epoch in range(N_EPOCHS):
 
        if(i+1)%100==0:
            print(f'Epoch[{epoch+1}/{N_EPOCHS}],Step[{i+1}/{n_total_steps}], Loss:{loss.item():.4f}')
-
-    with torch.no_grad():
-        n_correct=0
-        n_samples=0
-        for i,(images, labels) in enumerate (valid_loader):
-            images=images.to(DEVICE)
-            labels=labels.to(DEVICE)
-            outputs=model(images)
-            _,predicted=torch.max(outputs,1)
-            n_samples+=labels.size(0)
-            n_correct+=(predicted==labels).sum().item()
-            acc=100*n_correct/n_samples
-        print(f'Accuracy of the network:{acc}%')
 print('Finished Training')
 
+with torch.no_grad():
+    n_correct=0
+    n_samples=0
+    for i,(images, labels) in enumerate (valid_loader):
+        images=images.to(DEVICE)
+        labels=labels.to(DEVICE)
+        outputs=model(images)
+        _,predicted=torch.max(outputs,1)
+        n_samples+=labels.size(0)
+        n_correct+=(predicted==labels).sum().item()
+        acc=100*n_correct/n_samples
+    print(f'Accuracy of the network:{acc}%')
