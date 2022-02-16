3e24c4c7a082f2ccf7230d9090f98e4916e7aaef
Victor-Gao <gxh.vic@gmail.com>
2022-02-15 22:23:28 -0400

    before extract weight

--- load_saved_model.py
+++ load_saved_model.py
@@ -16,5 +16,26 @@ with torch.no_grad():
         _,predicted=torch.max(outputs,1)
         n_samples+=labels.size(0)
         n_correct+=(predicted==labels).sum().item()
-    acc=100*n_correct/n_samples
+    acc=100*n_correct/n_samples#->9860/10000
     print(f'Accuracy of the network:{acc}%')
+
+#### EXTRACT and MODIFY weight
+def weight_ex_ex(model):
+    
+    print(type(model))
+
+    return model
+
+with torch.no_grad():
+    n_correct=0
+    n_samples=0
+    for images,labels in valid_loader:
+        images=images.to(DEVICE)
+        labels=labels.to(DEVICE)
+        new_model=weight_ex_ex(loaded_model)
+        outputs=loaded_model(images)
+        _,predicted=torch.max(outputs,1)
+        n_samples+=labels.size(0)
+        n_correct+=(predicted==labels).sum().item()
+    acc=100*n_correct/n_samples
+    print(f'Accuracy of the network:{acc}%, correct:{n_correct}, total:{n_samples}')
