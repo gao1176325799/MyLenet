e596bf246a68d0ee2cff75e364895f2df6f9446c
Victor-Gao <gxh.vic@gmail.com>
2022-02-22 10:44:59 -0400

    Feb 20
    固定版本

--- load_saved_model.py
+++ load_saved_model.py
@@ -6,7 +6,37 @@ import matplotlib.pyplot as plt
 loaded_model=load_all(FILE)
 #loaded_model.eval()
 print(loaded_model)
+def plot(name, param):
+    dims = len(param[1].shape)
+    y = param.detach().cpu().numpy().reshape(-1)
+    x = range(len(y))
 
+    y_min = min(y)
+    x_min = 0
+    for i, v in enumerate(y):
+        if v == y_min:
+            x_min = i
+            break
+
+    y_max = max(y)
+    x_max = 0
+    for i, v in enumerate(y):
+        if v == y_max:
+            x_max = i
+            break
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    l1 = plt.scatter(x, y, marker='.')
+    l2 = plt.scatter(x_min, y_min, marker='x', color='red')
+    l3 = plt.scatter(x_max, y_max, marker='x', color='red')
+
+    plt.legend(handles=[l1, l2, l3], labels=[name, 'min: ' + str(y_min), 'max: ' + str(y_max)], loc='best')
+
+    
+
+    plt.show()
 with torch.no_grad():
         n_correct=0
         n_samples=0
@@ -20,17 +50,13 @@ with torch.no_grad():
         acc=100*n_correct/n_samples#->9860/10000
         print(f'Accuracy of the network:{acc}%')
 
-
-
-
-
-
 test0=1
 
 if test0:
     with torch.no_grad():
         for name, param in loaded_model.named_parameters():
             if name=='conv1.weight':
+                plot(name,param)
                 sum1=0
                 sum2=0
                 out_f,in_f,k_H,k_W=param.shape
@@ -48,12 +74,13 @@ if test0:
                     for k in range(in_f):
                         for j in range(k_H):
                             for l in range(k_W):
-                                param[i][k][j][l]=(param[i][k][j][l]+0.060)/2
+                                param[i][k][j][l]=(param[i][k][j][l]+2.5)/3.5
                 p=param.detach().to('cpu').numpy()
                 p=np.reshape(p,(1,total_num_weight))
                 for i in range(total_num_weight):
                     sum2+=p[0][i]
                 avg2=sum2/total_num_weight
+                plot(name,param)
                 print('after modify avg is:',avg2)
         n_correct=0
         n_samples=0
