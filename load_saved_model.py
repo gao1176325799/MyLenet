e92bb5ebbb98c38408cceb5b06633be950d968ef
AI_Cracker <gxh.vic@gmail.com>
2022-02-25 00:23:04 -0400

    2.25

--- load_saved_model.py
+++ load_saved_model.py
@@ -50,8 +50,57 @@ with torch.no_grad():
         acc=100*n_correct/n_samples#->9860/10000
         print(f'Accuracy of the network:{acc}%')
 
-test0=1
+def search():
+    pass
 
+def para_m(parameters):
+    test=[0,0,0,0]
+    with torch.no_grad():
+        for name, param in parameters:
+            if name=='conv1.weight':#later it will be all the weight
+                print(f'Before modify')
+                plot(name,param)
+                sum1=0
+                sum2=0
+                out_f,in_f,k_H,k_W=param.shape
+                print(out_f,in_f,k_H,k_W)
+                total_num_weight=out_f*in_f*k_H*k_W
+            
+                print(param[0][0][0][0].detach().to('cpu').numpy())
+                p=param.detach().to('cpu').numpy()
+                p=np.reshape(p,(1,total_num_weight))
+                for i in range(total_num_weight):
+                    sum1+=p[0][i]
+                avg1=sum1/total_num_weight
+                print('before modify the avg is:',avg1)
+                for i in range(out_f):
+                    for k in range(in_f):
+                        for j in range(k_H):
+                            for l in range(k_W):
+                                param[i][k][j][l]=(param[i][k][j][l]+2.5)/3.5
+                p=param.detach().to('cpu').numpy()
+                p=np.reshape(p,(1,total_num_weight))
+                for i in range(total_num_weight):
+                    sum2+=p[0][i]
+                avg2=sum2/total_num_weight
+                plot(name,param)
+                print('after modify avg is:',avg2)
+        n_correct=0
+        n_samples=0
+        for i,(images, labels) in enumerate (valid_loader):
+            images=images.to(DEVICE)
+            labels=labels.to(DEVICE)
+            outputs=loaded_model(images)
+            _,predicted=torch.max(outputs,1)
+            n_samples+=labels.size(0)
+            n_correct+=(predicted==labels).sum().item()
+        acc=100*n_correct/n_samples#->9860/10000
+        print(f'Accuracy of the network:{acc}%')   
+
+para_m(loaded_model.named_parameters())
+
+
+test0=0
 if test0:
     with torch.no_grad():
         for name, param in loaded_model.named_parameters():
